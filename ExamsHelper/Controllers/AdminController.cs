036c301cc669ade6f39a7adf795bd68e9cd60fbc
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-07-26 19:57:25 +0300

    added all admin endpoints withput return views

--- ExamsHelper/Controllers/AdminController.cs
+++ ExamsHelper/Controllers/AdminController.cs
@@ -23,6 +23,48 @@ namespace ExamsHelper.Controllers
             unvS = new UniversityService(context);
         }
 
+        public IActionResult DeleteUser(int id)
+        {
+            uS.deleteUser(id);
+            return View();
+        }
+
+        public IActionResult Admin(int id)
+        {
+            uS.makeAdmin(id);
+            return View();
+        }
+
+        public IActionResult NoAdmin(int id)
+        {
+            uS.noAdmin(id);
+            return View();
+        }
+
+        public IActionResult Moder(int id)
+        {
+            uS.makeModer(id);
+            return View();
+        }
+
+        public IActionResult NoModer(int id)
+        {
+            uS.noModer(id);
+            return View();
+        }
+
+        public IActionResult DeleteUniversity(int id)
+        {
+            unvS.deleteUniver(id);
+            return View();
+        }
+
+        public IActionResult DeleteFaculty(int id)
+        {
+            fS.deleteFaculty(id);
+            return View();
+        }
+
         public IActionResult Index()
         {
             if (uS.checkAdmRole(User.Identity.Name))
