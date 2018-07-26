8f38e26a9933879b4d3382cbeaac3463caafb4e6
Unknown <mushuk-artur@mail.ru>
2018-07-26 22:20:05 +0300

    Admin panel is done.

--- ExamsHelper/Controllers/AdminController.cs
+++ ExamsHelper/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ExamsHelper.Context;
 using ExamsHelper.Models;
+using ExamsHelper.ViewModels;
 using ExamsHelper.Services;
 using Microsoft.AspNetCore.Mvc;
 
--- ExamsHelper/ViewModels/AdminViewModel.cs
+++ ExamsHelper/ViewModels/AdminViewModel.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExamsHelper.Models;
 
-namespace ExamsHelper.Models
+namespace ExamsHelper.ViewModels
 {
     public class AdminViewModel
     {
