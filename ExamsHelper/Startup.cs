3b84b019077bf8157636036d7ff9c8cdc7e2c2c2
Dagasson <mushuk-artur@mail.ru>
2018-07-28 22:21:23 +0300

    Fixed Azure's problem with db

--- ExamsHelper/Startup.cs
+++ ExamsHelper/Startup.cs
@@ -26,15 +26,15 @@ namespace ExamsHelper
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connection = Configuration.GetConnectionString("DefaultConnection");
             
-
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            string connection = Configuration.GetConnectionString("LocaldbConnection");
+            string azureconn = Configuration.GetConnectionString("SQLServerConnection");
+           // if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            //    services.AddDbContext<dbcontext>(options =>
+            //            options.UseSqlServer(Configuration.GetConnectionString("SQLServerConnection")));//SQLServerConnection
+          //  else
                 services.AddDbContext<dbcontext>(options =>
-                        options.UseSqlServer(Configuration.GetConnectionString("SqlServerConnection")));
-            else
-                services.AddDbContext<dbcontext>(options =>
-                options.UseSqlServer(connection));
+                options.UseSqlServer(azureconn));
 
             services.BuildServiceProvider().GetService<dbcontext>().Database.Migrate();
 
